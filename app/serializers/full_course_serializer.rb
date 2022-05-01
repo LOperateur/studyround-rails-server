1596af91aef4e53654896e208218144e691b1d23
mofeejegi <mofeejegi@gmail.com>
2022-05-01 17:27:54 +0100

    Full course about attribute hot fix

--- app/serializers/full_course_serializer.rb
+++ app/serializers/full_course_serializer.rb
@@ -1,6 +1,6 @@
 class FullCourseSerializer < CourseSerializer
   type :course
-  attributes :review_count, :instructions, :test_expiration, :private
+  attributes :about, :review_count, :instructions, :test_expiration, :private
 
   has_many :categories, serializer: MiniCategorySerializer
 
