a50a5e56a595bc487b7aa9709a37b6e4f1ddb7a2
mofeejegi <mofeejegi@gmail.com>
2022-05-08 19:26:23 +0100

    Session helper hotfix

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,21 @@
+module SessionHelper
+  def session_id_to_seed(id)
+    # If the id is nil or it doesn't contains only numbers
+    if id.nil? || !id.to_s.scan(/\D/).empty?
+      SecureRandom.random_number
+    else
+      ("0." + id.to_s).to_f
+    end
+  end
+
+  def session_type(session_type)
+    if session_type.nil? || !session_types.include?(session_type.to_sym)
+      raise Errors::BaseError.new(message: "Invalid session type")
+    end
+    session_type.to_sym
+  end
+
+  def session_types
+    [:quiz, :practice, :study]
+  end
+end
