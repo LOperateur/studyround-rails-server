58503e1237cbbd6715cce2dbd21218160f87b95a
mofeejegi <mofeejegi@gmail.com>
2022-05-12 12:57:33 +0100

    Added elapsed time to result

--- app/serializers/result_serializer.rb
+++ app/serializers/result_serializer.rb
@@ -1,5 +1,5 @@
 class ResultSerializer < ActiveModel::Serializer
-  attributes :id, :score, :total, :percent, :created_at, :course
+  attributes :id, :score, :total, :percent, :elapsed_time, :created_at, :course
 
   def percent
     ((object.score.to_f / object.total.to_f) * 100).round(2)
