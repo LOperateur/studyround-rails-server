5e6f45cd48860cdb3a0b8ed683f3a08e7d7ee589
mofeejegi <mofeejegi@gmail.com>
2022-05-28 19:49:06 +0100

    Get secret key from credentials instead of secrets

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,11 +2,13 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 1.day.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secret_key_base)
+      # JWT.encode(payload, Rails.application.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.dig(Rails.env.to_sym, :secret_key_base))
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secret_key_base)[0]
+      # body = JWT.decode(token, Rails.application.secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.credentials.dig(Rails.env.to_sym, :secret_key_base))[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
