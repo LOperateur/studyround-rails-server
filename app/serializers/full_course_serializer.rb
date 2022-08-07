9991414789f0ba60fe2dff02d8ec7d2b34195f01
mofeejegi <mofeejegi@gmail.com>
2022-08-07 23:54:04 +0100

    Implemented close test endpoint

--- app/serializers/full_course_serializer.rb
+++ app/serializers/full_course_serializer.rb
@@ -1,27 +1,24 @@
 class FullCourseSerializer < DetailedCourseSerializer
   type :course
-  attributes :test_expiration, :private, :publish_status,
-             :course_status, :test_users, :test_submissions, :test_result_expiration
+  attributes :instructions, :private, :publish_status, :course_status, :test_statistics
 
-  def test_users
+  def test_statistics
     if object.test
-      Result.where(course_id: object.id).distinct.count(:user_id)
-    else
-      nil
-    end
-  end
 
-  def test_submissions
-    if object.test
-      Result.where(course_id: object.id).count
-    else
-      nil
-    end
-  end
+      expiration = object.test_expiration
+      results = Result.where(course_id: object.id)
+      lag_time = 1.hour
+      closing_time = expiration + (object.instructions['time']).seconds + lag_time
+      is_closeable = closing_time < Time.now
 
-  def test_result_expiration
-    if object.test
-      object.test_expiration + 48.hours
+      return {
+        expiration: expiration,
+        users: results.distinct.count(:user_id),
+        submissions: results.count,
+        result_expiration: if object.creator.pro_account then nil else expiration + 48.hours end,
+        closing_time: closing_time,
+        closeable: is_closeable,
+      }
     else
       nil
     end
