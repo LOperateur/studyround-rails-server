a52124d9fb5c341eda39fd0ab5debe33ebcb3c92
mofeejegi <mofeejegi@gmail.com>
2022-11-20 21:43:28 +0100

    Added user update and image upload functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   before_save :downcase_fields
   validates :username, presence: true, uniqueness: true, format: { with: /\A[-a-z0-9_.]+\Z/i }
   validates :email, presence: true, uniqueness: true
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
+  validates :password_confirmation, presence: true, length: { minimum: 8 }, allow_nil: true
 
   has_one :refresh_token, dependent: :destroy
   has_many :courses, dependent: :destroy, class_name: "Course", foreign_key: :creator_id
@@ -16,6 +18,7 @@ class User < ApplicationRecord
   has_many :results
   has_many :reviews
   has_many :sessions
+  has_one_attached :profile_image
 
   def downcase_fields
     self.username.downcase!
@@ -28,4 +31,12 @@ class User < ApplicationRecord
     # ActiveModelSerializers::SerializableResource.new(self, serializer: UserSerializer, root: :profile).as_json
   end
 
+  def generated_profile_image_url
+    begin
+      path = rails_blob_path(self.profile_image, only_path: true)
+      return ActionController::Base.helpers.asset_path(path)
+    rescue
+      nil
+    end
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,8 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :first_name, :last_name,
              :other_name, :date_of_birth, :creator, :pro_account,
              :occupation, :country, :profile_image_url, :about
+
+  def profile_image_url
+    object.generated_profile_image_url
+  end
 end
