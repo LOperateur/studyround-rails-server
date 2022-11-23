8a74693287a275e5c7576e870d49aca1d48f0d04
Mofe Ejegi <mofeejegi@gmail.com>
2022-11-23 23:38:19 +0100

    Merge Development to AWS Staging (#61)

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,8 @@
 class ProfileSerializer < ActiveModel::Serializer
+  type :user
   attributes :id, :username, :profile_image_url, :first_name, :last_name, :email, :pro_account, :country
+
+  def profile_image_url
+    object.generated_profile_image_url
+  end
 end
