84405f89e259003032dede726ababdb7f397d61c
mofeejegi <mofeejegi@gmail.com>
2022-12-11 20:47:58 +0100

    Rough setup for ENV vars from AWS/Docker

--- config/application.rb
+++ config/application.rb
@@ -35,13 +35,6 @@ module ULearnserver
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
-
     if Rails.env.production?
       # TODO: Set rails config with required origin
     else
