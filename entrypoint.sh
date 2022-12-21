4151305fe1f9a0dbb3bb14eda8602e52afc8eb11
Caleb Ewere Ebie <ewere.ebie@ottonova.de>
2022-12-21 21:32:17 +0100

    create local env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-echo $JSON_VAR | jq -r 'to_entries[] | .key + "=\"" + (.value|tostring) + "\""' > "file.txt" && set -a && . ./file.txt && set +a
+echo $JSON_VAR | jq -r 'to_entries[] | .key + ":'" + (.value|tostring) + "'"' > "config/local_env.yml" && set -o allexport; source ./config/local_env.yml; set +o allexport
 
 bundle exec rake db:migrate
 
