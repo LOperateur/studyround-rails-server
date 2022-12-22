14aa3a55de748942bbf8e478bb0b687b5bd6aac5
Caleb Ewere Ebie <ewere.ebie@ottonova.de>
2022-12-22 01:51:18 +0100

    clean env generatio

--- sidekiq.sh
+++ sidekiq.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-echo $JSON_VAR | jq -r 'to_entries[] | .key + ":'" + (.value|tostring) + "'"' > "config/local_env.yml" && set -o allexport; source ./config/local_env.yml; set +o allexport
+echo $JSON_VAR | jq -r 'to_entries[] | .key + ": \"" + (.value|tostring) + "\""' > "config/local_env.yml" && set -o allexport; . ./config/local_env.yml; set +o allexport
 
 bundle exec sidekiq -c 5 -q default -q mailers -v
