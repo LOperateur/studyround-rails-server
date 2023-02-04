b4ce7efe5a4a48effc637ec37945463b131acd93
mofeejegi <mofeejegi@gmail.com>
2023-02-04 13:39:26 +0100

    Mask Card PAN and return it in API

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,3 +1,8 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id, :country, :expiry, :first_six, :last_four, :card_type
+  attributes :id, :masked_pan, :country, :expiry, :card_type
+
+  def masked_pan
+    "#{object.first_six[0..3]}-#{object.first_six[4..-1]}XX-XXXX-#{object.last_four}"
+  end
+
 end
