e4a3f0c7b039b77e38ad7060ac28901128b7a4cd
mofeejegi <mofeejegi@gmail.com>
2023-04-23 23:49:17 +0100

    Prevent outside access to draft courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -29,8 +29,8 @@ class CoursesController < ApplicationController
     end
 
     if current_user.nil? || (@course.creator != current_user && current_user.user_type != :admin)
-      if @course.course_status_suspended? || @course.course_status_closed?
-        raise Errors::ForbiddenError.new(message: "This #{course_or_test(@course)} is unavailable")
+      if @course.publish_status_draft? || @course.course_status_suspended? || @course.course_status_closed?
+        raise Errors::ForbiddenError.new(message: "This #{course_or_test(@course)} is currently unavailable")
       end
 
       render json: { data: @course.serialized_user_facing_course[:course].merge(purchase_status: purchase_status) }
