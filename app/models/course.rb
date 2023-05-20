5af67a6067570851fb2a056a4610b1961e264fd8
mofeejegi <mofeejegi@gmail.com>
2023-05-20 21:24:31 +0100

    Use delete_all for course dependent questions

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,7 +9,7 @@ class Course < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
   has_many :results
-  has_many :questions, dependent: :destroy
+  has_many :questions, dependent: :delete_all # Doing this instead of destroy because questions are self-referential
   has_many :reviews, dependent: :destroy
   has_many :sessions
   has_one_attached :image
