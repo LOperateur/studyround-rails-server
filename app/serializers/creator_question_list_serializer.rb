813705dda2be9f52a2b7b5dc9c4653fa795d9bbd
mofeejegi <mofeejegi@gmail.com>
2023-05-21 12:26:39 +0100

    Added creator object to creator question list

--- app/serializers/creator_question_list_serializer.rb
+++ app/serializers/creator_question_list_serializer.rb
@@ -2,6 +2,8 @@ class CreatorQuestionListSerializer < ActiveModel::Serializer
   type :question
   attributes :id, :question, :draft, :notes
 
+  belongs_to :creator, serializer: MiniProfileSerializer
+
   def draft
     if object.draft.present?
       { question: object.draft["question"] }
