a758b35bead31abf21a45791e0d88f97a261791a
mofeejegi <mofeejegi@gmail.com>
2023-05-22 18:35:16 +0100

    Automation controls for course assignment

---
+++ app/controllers/automation_controller.rb
@@ -0,0 +1,38 @@
+class AutomationController < ApplicationController
+  skip_before_action :authorize!
+
+  def assign_course
+    creator_username, reviewer_username = parse_trello_course_desc(assign_course_params[:description])
+
+    creator = User.find_by(username: creator_username)
+    reviewer = User.find_by(username: reviewer_username)
+
+    assignment = assign_course_params[:assign_to]
+    course = Course.non_deleted_courses.find_by!(title: assign_course_params[:course])
+
+    if assignment == "creator"
+      course.creator = creator
+    elsif assignment == "reviewer"
+      course.creator = reviewer
+    end
+
+    course.save!
+
+    render json: {}, root: :data, status: :ok
+  end
+
+  private
+
+  def parse_trello_course_desc(string)
+    pattern = /\[(.*?)\]/
+    creator, reviewer = string.split("\n").map do |line|
+      match = line.match(pattern)
+      match[1] if match
+    end
+    [creator, reviewer]
+  end
+
+  def assign_course_params
+    params.permit(:course, :description, :assign_to)
+  end
+end
