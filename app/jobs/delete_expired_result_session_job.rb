d4e01b728ea89100461a4304587d5607e292c514
mofeejegi <mofeejegi@gmail.com>
2023-06-25 01:25:51 +0100

    Restrict guest data to 30 days and cleanup afterwards

--- app/jobs/delete_expired_result_session_job.rb
+++ app/jobs/delete_expired_result_session_job.rb
@@ -4,5 +4,8 @@ class DeleteExpiredResultSessionJob < ApplicationJob
   def perform
     # Remove the session_items from Results that are older than 30 days and where it is not a test
     Result.where("created_at < ?", Time.now - 30.days).where.not(session_type: :test).update_all(session_items: nil)
+
+    # Also remove the results from Guests that are older than 30 days
+    Guest.where("created_at < ?", Time.now - 30.days).update_all(result: nil)
   end
 end
