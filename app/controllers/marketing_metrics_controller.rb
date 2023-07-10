f6b612cc0f2932c308e894d520ea896884d47786
caleb-42 <ewisco316@yahoo.com>
2023-07-10 22:04:28 +0200

    implement feedback

--- app/controllers/marketing_metrics_controller.rb
+++ app/controllers/marketing_metrics_controller.rb
@@ -1,19 +1,15 @@
-require 'net/http'
-require 'json'
 
 class MarketingMetricsController < ApplicationController
   def source
-    metrics_endpoint = ENV["MARKETING_METRICS_ENDPOINT"] + "/marketing-source"
-    url = URI.parse(metrics_endpoint)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+    conn = Faraday.new(
+      url: ENV["MARKETING_METRICS_ENDPOINT"],
+      headers: { 'Content-Type' => 'application/json' }
+    )
+    post_data = { username: current_user.username, source: params[:source] }
+    response = conn.post("/v1/marketing-source") do |req|
+      req.body = { username: current_user.username, source: params[:source] }.to_json
+    end
 
-    request = Net::HTTP::Post.new(url.path, { 'Content-Type' => 'application/json' })
-    request.body = { username: current_user.username, source: params[:source] }.to_json
-
-    response = http.request(request)
-    data = JSON.parse(response.body)
-
-    render json: data
+    render json: response.body
   end
 end
