0ceeb754e47c3906ec613d01df021be6259ed52c
Mofe Ejegi <mofeejegi@gmail.com>
2023-08-10 13:44:23 +0100

    Allow Fuzzy Search with Configurable Strict Param (#203)

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -440,7 +440,10 @@ class CoursesController < ApplicationController
     search_query = params[:q] || nil
     category_filters = params[:category] || []
     creator_filters = params[:creator] || []
-    test_filter = params[:test] || nil
+    test_filter = params[:test] == "true"
+
+    # If strict is set to true, we will only return courses that EXACTLY match the category or creator filters
+    strict_match = params[:strict] == "true"
 
     found_courses = courses
 
@@ -450,16 +453,28 @@ class CoursesController < ApplicationController
     end
 
     if category_filters.present?
-      category_ids = Category.select(:id).where(name: category_filters)
+      if strict_match
+        category_ids = Category.select(:id).where(name: category_filters)
+      else
+        # "name ILIKE ? OR name ILIKE ? OR name ILIKE ? ..."
+        category_conditions = category_filters.map { "name ILIKE ?" }.join(' OR ')
+        category_ids = Category.select(:id).where(category_conditions, *category_filters.map { |filter| "%#{filter}%" }).limit(10)
+      end
       found_courses = found_courses.filtered_by_category(category_ids)
     end
 
     if creator_filters.present?
-      creator_ids = User.select(:id).where(username: creator_filters)
+      if strict_match
+        creator_ids = User.select(:id).where(username: creator_filters)
+      else
+        # "username ILIKE ? OR username ILIKE ? OR username ILIKE ? ..."
+        creator_conditions = creator_filters.map { "username ILIKE ?" }.join(' OR ')
+        creator_ids = User.select(:id).where(creator_conditions, *creator_filters.map { |filter| "%#{filter}%" }).limit(10)
+      end
       found_courses = found_courses.filtered_by_creators(creator_ids)
     end
 
-    if !test_filter.nil?
+    if params.key?(:test)
       found_courses = found_courses.filtered_by_test(test_filter)
     end
 
