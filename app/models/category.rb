942ae899a5b17aca7ece8d022f49c7c28bf9b9e9
Mofe Ejegi <mofeejegi@gmail.com>
2023-08-25 11:15:54 +0100

    Remove parent category reference and planned out new categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,12 +2,23 @@ class Category < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  belongs_to :parent, class_name: 'Category', foreign_key: :parent_id, optional: true
-
   has_many :interests, dependent: :destroy
   has_many :users, through: :interests
   has_many :categorizations, dependent: :destroy
   has_many :courses, through: :categorizations
+
+  # Here are our current category definitions across levels:
+  # More categories can be added as needed...
+  # {
+  #   "category_levels": [
+  #       "1-faculty": ["General Knowledge","Engineering","Agriculture","Earth","Sciences","Legal",
+  #                     "Arts & Humanities","Finance","Social Sciences","Technology","International","IQ"],
+  #       "2-examinations": ["JAMB","P-UTME","WAEC"],
+  #       "3-institutions": ["UNIBEN","DELSU"],
+  #       "4-sectors": ["Banking","Tech","Fashion"],
+  #       "5-institution-type": ["University","College","Polytechnic","Secondary","Primary","Adult School"]
+  #   ]
+  # }
 
   # Used to serialize the category model for categorised courses on the go without having to render
   def serialized_categorised_course
