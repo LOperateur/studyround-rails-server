75d3fc62f3fc030aa3486e2a9b0388d927fcaac6
Mofe Ejegi <mofeejegi@gmail.com>
2023-08-26 12:24:05 +0100

    Categorised courses behaviour standardization

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,13 +7,15 @@ class Category < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :courses, through: :categorizations
 
+  scope :published_active_course_categories, -> { joins(:courses).where(courses: { publish_status: :publish_status_published, course_status: :course_status_active, private: false }) }
+
   # Here are our current category definitions across levels:
   # More categories can be added as needed...
   # {
   #   "Occupation": "Student" | "Professional" | "Default"
-  #   "Student order": "5, 3, 2, 1, 4".
-  #   "Professional order": "4, 1, 5, 3, 2".
-  #   "Default order": "1, 4, 5, 2, 3".
+  #   "Student order": "5, 3, 2, 1, 4"
+  #   "Professional order": "4, 1, 5, 3, 2"
+  #   "Default order": "1, 4, 5, 2, 3"
   #
   #   "category_levels": [
   #       "1-faculty": ["General Knowledge","Engineering","Medical Sciences","Agriculture","Sciences","Legal","Arts & Humanities","Business","Social Sciences","Education","Environmental Sciences"],
