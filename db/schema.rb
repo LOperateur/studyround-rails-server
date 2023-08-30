781562fda7a23a8cd60ba4a701d15f1db1a3aadb
Mofe Ejegi <mofeejegi@gmail.com>
2023-08-30 15:02:26 +0100

    Include Asset Names and Other Fixes (#234)
    * Return username and id in social auth redirect
    
    * Include name for assets
    
    * Fix assets pagination
    
    * Optimized update of passage content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_25_093849) do
+ActiveRecord::Schema.define(version: 2023_08_30_111539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2023_08_25_093849) do
     t.integer "asset_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["course_id"], name: "index_question_assets_on_course_id"
   end
 
