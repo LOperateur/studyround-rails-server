e6bce2449f571147aca4388c9d8ab3e450112b76
Mofe Ejegi <mofeejegi@gmail.com>
2024-04-14 22:05:54 +0100

    Make courses private by default for non-admins

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -64,9 +64,9 @@ class CoursesController < ApplicationController
 
     course_params = prepare_received_course_params(create_course_params)
     course = current_user.courses.build(course_params)
-    course.private = true if current_user.user_type == :admin
+    course.private = true if current_user.user_type != :admin # Temp solution
 
-    course.save!
+    # course.save!
     render json: course, root: :data, serializer: CreatorCourseSerializer
   end
 
@@ -101,6 +101,7 @@ class CoursesController < ApplicationController
 
     handle_image_update(course_params)
     @course.assign_attributes(course_params.except(:image_url))
+    @course.private = true if current_user.user_type != :admin # Temp solution
 
     @course.save!
     render json: @course, root: :data, serializer: CreatorCourseSerializer
