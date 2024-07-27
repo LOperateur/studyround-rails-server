d0017a034d8960526765303f7caeeda008234ecb
Mofe Ejegi <mofeejegi@gmail.com>
2024-07-27 13:27:08 +0100

    Include dummy courses in main course index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,7 +13,7 @@ class CoursesController < ApplicationController
   wrap_parameters format: []
 
   def index
-    found_courses = search_and_filter(Course.published_active_courses.ordered_by_result_count)
+    found_courses = search_and_filter(Course.published_active_or_dummy_courses.ordered_by_result_count)
 
     # Specifying entry count here due to the result group count query which returns a hash of { grouped courses -> result count }
     # https://api.rubyonrails.org/v7.0.6/classes/ActiveRecord/Calculations.html#method-i-count
@@ -437,7 +437,7 @@ class CoursesController < ApplicationController
 
   def dummy_courses
     # Get dummy courses for user feedback
-    courses = Course.where(course_status: :course_status_dummy).order(created_at: :desc)
+    courses = Course.dummy_courses.order(created_at: :desc)
 
     paginated_courses = paginate(courses, params)
     render json: paginated_courses, root: :data, meta: paginated_meta(paginated_courses)
