48cb0ff4321b772a53c85c1cc226403242866eac
Mofe Ejegi <mofeejegi@gmail.com>
2024-10-22 20:48:32 +0100

    Unify Transaction Process endpoint (#388)
    * Unify Transaction Process endpoint
    
    * Handle Card Types more Robustly

--- app/controllers/concerns/card_transaction_helper.rb
+++ app/controllers/concerns/card_transaction_helper.rb
@@ -2,19 +2,7 @@ module CardTransactionHelper
   extend ActiveSupport::Concern
 
   def process_card_transaction(item_id, card_id, item_type)
-    begin
-      card = current_user.financial_cards.find(card_id)
-    rescue
-      raise Errors::NotFoundError.new(message: "Unable to find payment method attached")
-    end
-
-    if card.is_flutterwave_card?
-      transactions_controller = FlutterwaveTransactionsController.new
-    elsif card.is_paystack_card?
-      transactions_controller = PaystackTransactionsController.new
-    else
-      raise Errors::BaseError.new(message: "Unsupported payment method", status: 400)
-    end
+    transactions_controller = TransactionsController.new
 
     transactions_controller.request = request
     transactions_controller.response = response
