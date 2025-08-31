2767dfed4b26ef1eebaaf3e3464a6c59bfe8547a
Mofe Ejegi <mofeejegi@gmail.com>
2025-09-01 00:01:03 +0100

    Update SMTP settings to use environment variables for mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
+    address:              ENV["SMTP_SERVER"],
+    port:                 ENV["SMTP_PORT"].to_i,
     domain:               'studyround.com',
-    user_name:            'apikey',
-    password:             ENV["SENDGRID_API_KEY"],
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
